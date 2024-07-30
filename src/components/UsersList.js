a5a2f46f45240e350c84f42076f1b7e172f28745
Haseebj53 <haseebj53@gmail.com>
2024-07-31 03:46:49 +1000

    Async Thunk APi Fetching

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,14 +1,26 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers } from "../store";
-
+import Skeleton from "./Skeleton";
 function UsersList() {
+  const { isLoading, data, error } = useSelector((state) => {
+    return state.users;
+  });
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchUsers());
   }, [dispatch]);
-  return "UsersList";
+  if (isLoading) {
+    return <Skeleton times={6} className="h-10 w-full" />;
+  }
+  if (error) {
+    return <div>Error Fetching Data...</div>;
+  }
+  // if (data) {
+  //   return <div>{data}</div>;
+  // }
+  return <div>{data.length}</div>;
 }
 
 export default UsersList;
