4a038c0f6e0ee33fbeed7eac261a37e4ede4e25b
Haseebj53 <haseebj53@gmail.com>
2024-07-31 14:02:54 +1000

    Fetch Albums

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,4 +1,10 @@
+import { useFetchAlbumsQuery } from "../store";
+
 function AlbumsList({ user }) {
+  //   const { data, error, isLoading } = useFetchAlbumsQuery(user);
+  const result = useFetchAlbumsQuery(user);
+
+  console.log(result);
   return <div>Albums For {user.name}</div>;
 }
 
