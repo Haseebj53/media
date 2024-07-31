6e466bf9faf9c4c5f58443eef387b2fffa3bae4e
Haseebj53 <haseebj53@gmail.com>
2024-07-31 14:02:36 +1000

    Added RTK Query

--- src/components/UsersListItem.js
+++ src/components/UsersListItem.js
@@ -2,27 +2,26 @@ import Button from "./Button";
 import { GoTrashcan } from "react-icons/go";
 import { removeUser } from "../store";
 import { useThunk } from "../hooks/use-thunk";
-
+import ExpandablePanel from "./ExpandablePanel";
+import AlbumbsList from "./AlbumsList";
 function UsersListItem({ user }) {
   const [doRemoveUser, isLoading, error] = useThunk(removeUser);
   const handleRemoveUser = () => {
     doRemoveUser(user);
   };
+  const header = (
+    <>
+      <Button className="mr-3" loading={isLoading} onClick={handleRemoveUser}>
+        <GoTrashcan />
+      </Button>
+      {error && <div>Error deleting user...</div>}
+      {user.name}
+    </>
+  );
   return (
-    <div className="mb-2 border rounded">
-      <div className="flex p-2 justify-between items-center cursor-pointer">
-        <div className="flex flex-row items-center justify-between">
-          <Button
-            className="mr-3"
-            loading={isLoading}
-            onClick={handleRemoveUser}>
-            <GoTrashcan />
-          </Button>
-          {error && <div>Error deleting user...</div>}
-          {user.name}
-        </div>
-      </div>
-    </div>
+    <ExpandablePanel header={header}>
+      <AlbumbsList user={user} />
+    </ExpandablePanel>
   );
 }
 
